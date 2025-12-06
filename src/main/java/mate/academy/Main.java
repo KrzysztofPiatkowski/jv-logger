83edef56d7d7b97d5991f566a6e1f3ff51407611
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-12-06 15:44:11 +0100

    Add fixes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -16,10 +16,12 @@ public class Main {
 
         AuthenticationService authenticationService = new AuthenticationServiceImpl();
         User user;
+        String login = "bob";
+
         try {
-            user = authenticationService.login("bob", "1234");
+            user = authenticationService.login(login, "1234");
         } catch (AuthenticationException e) {
-            logger.error("Authentication failed ", e);
+            logger.error("Authentication failed for user '{}'", login, e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
